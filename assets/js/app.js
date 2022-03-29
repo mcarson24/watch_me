4dd761366a73df98e65bfbe26b769754862e38fa
Matthew Carson <matthew.carson@pm.me>
2022-03-29 13:17:31 -0400

    Finish user input WatchMode search
    The user will click on one of the titles returned from the search and that will trigger an API call to WatchMode to get the information on that title.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
+const searchForm = document.querySelector('#search')
+const searchResults = document.querySelector('#search-results')
+
+let titleInfo
+
 // TODO: GET Info on TV Show or Movie from IMDB API
 
 const getTitleInformation = async title => {
@@ -5,6 +10,7 @@ const getTitleInformation = async title => {
   // IMDb API 'SeachTitle' endpoint
   let response = await fetch(`https://imdb-api.com/en/API/SearchTitle/k_vd8o64bn/${title.trim()}`)
   let data = await response.json()
+  displaySearchResults(data)
 
   // No data is found matching query
   if (!data.results.length) {
@@ -16,15 +22,40 @@ const getTitleInformation = async title => {
   // Response returns an array of content. The user will select which movie/show they
   //  want; we will use that to make the next API call to watchMode.
   // Temporarily will use the first item returned.
-  const selected = data.results[0]
-
-  // WatchMode Title Details Endpoint
-  response = await fetch(`https://api.watchmode.com/v1/title/${selected.id}/details/?apiKey=oW3y8drDCyPOFpr3qmP4s0cWvaBFPPJ9x8SxXi5n&append_to_response=sources`)
-  return await response.json()
+  const selected = data.results[0] 
 }
 
-(async () => {
-  // Temporary title, will use user input soon.
-  const info = await getTitleInformation('The Shawshank Redemption')
-  console.log(info)
-})()
+const displaySearchResults = data => {
+  searchResults.classList.remove('hidden')
+  searchResults.innerHTML = ''
+
+  data.results.forEach(result => {
+    const li = document.createElement('li')
+    li.classList = 'even:bg-slate-200 px-2 py-4 font-bold hover:cursor-pointer'
+    li.textContent = result.title
+    li.dataset.imdb_id = result.id
+    searchResults.appendChild(li)
+  })
+
+  searchResults.addEventListener('click', async e => {
+    if (!e.target.matches('li')) return
+
+    // WatchMode Title Details Endpoint
+    response = await fetch(`https://api.watchmode.com/v1/title/${e.target.dataset.imdb_id}/details/?apiKey=oW3y8drDCyPOFpr3qmP4s0cWvaBFPPJ9x8SxXi5n&append_to_response=sources`)
+    titleInfo = await response.json()
+    
+    // Remove search results
+    resetSearchResults()
+  })
+}
+
+const resetSearchResults = () => {
+  searchResults.classList.add('hidden')
+  searchResults.parentElement.children[0].children[0].value = ''
+}
+
+searchForm.addEventListener('submit', e => {
+  e.preventDefault()
+
+  getTitleInformation(e.target.children[0].value)
+})

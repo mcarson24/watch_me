21c85593681b71b4da4217f4c64755a5098536f2
Matthew Carson <matthew.carson@pm.me>
2022-04-01 03:08:46 -0400

    I did a bunch of stuff and forgot to commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,19 +9,149 @@ var favorites = [];
 
 const watchModeKey = '9CXzPOxljYCpAhYv6DZM68hUPQ9ThHrdANojKj9w'
 
+const testData = {
+  "id": 1376730,
+  "title": "The Amazing Spider-Man 2",
+  "original_title": "The Amazing Spider-Man 2",
+  "plot_overview":null,
+  "type":"movie",
+  "runtime_minutes":null,
+  "year":2014,
+  "end_year":null,
+  "release_date":"2014-04-16",
+  "imdb_id":"tt1872181",
+  "tmdb_id":102382,
+  "tmdb_type":"movie",
+  "genres":[1,2,9],
+  "genre_names":["Action",
+  "Adventure",
+  "Fantasy"],
+  "user_rating":6.6,
+  "critic_score":52,
+  "us_rating":"PG-13",
+  "poster":"https:\/\/cdn.watchmode.com\/posters\/01376730_poster_w185.jpg",
+  "backdrop":"https:\/\/cdn.watchmode.com\/backdrops\/01376730_bd_w780.jpg",
+  "original_language":"en",
+  "similar_titles":[1431644,132421,1357311,1195407,1472955,1195406,1257219,1431651,1186053,1428390,1398669,144358,1501613,1567837,1406675,1207603,1455450,1392085,166940,1672769,1188812,1471731,1436507,1162636,1364053],
+  "networks":null,
+  "network_names":[],
+  "relevance_percentile":99.86,
+  "trailer":"https:\/\/www.youtube.com\/watch?v=nbp3Ra3Yp74",
+  "trailer_thumbnail":"https:\/\/cdn.watchmode.com\/video_thumbnails\/627969_pthumbnail_320.jpg",
+  "sources": [
+    {
+      "source_id":349,
+      "name":"iTunes",
+      "type":"rent",
+      "web_url":"https:\/\/itunes.apple.com\/us\/movie\/the-amazing-spider-man-2\/id854943940?uo=4&at=10laHb",
+      "format":"4K",
+      "price":3.99,
+      "seasons":null,
+      "episodes":null
+    },
+    {
+      "source_id":349,
+      "name":"iTunes",
+      "type":"rent",
+      "region":"US",
+      "ios_url":"Deeplinks available for paid plans only.",
+      "android_url":"Deeplinks available for paid plans only.",
+      "web_url":"https:\/\/itunes.apple.com\/us\/movie\/the-amazing-spider-man-2\/id854943940?uo=4&at=10laHb",
+      "format":"HD",
+      "price":3.99,
+      "seasons":null,
+      "episodes":null
+    },
+    {
+      "source_id":349,
+      "name":"iTunes",
+      "type":"rent",
+      "region":"US",
+      "ios_url":"Deeplinks available for paid plans only.",
+      "android_url":"Deeplinks available for paid plans only.",
+      "web_url":"https:\/\/itunes.apple.com\/us\/movie\/the-amazing-spider-man-2\/id854943940?uo=4&at=10laHb",
+      "format":"SD",
+      "price":3.99,
+      "seasons":null,
+      "episodes":null
+    },
+    {
+      "source_id":140,
+      "name":"Google Play",
+      "type":"rent",
+      "region":"US",
+      "ios_url":"Deeplinks available for paid plans only.",
+      "android_url":"Deeplinks available for paid plans only.",
+      "web_url":"https:\/\/play.google.com\/store\/movies\/details?id=rFN3fwhcdlE",
+      "format":"4K",
+      "price":3.99,
+      "seasons":null,
+      "episodes":null
+    },
+    {
+      "source_id":307,
+      "name":"VUDU",
+      "type":"rent",
+      "region":"US",
+      "ios_url":"Deeplinks available for paid plans only.",
+      "android_url":"Deeplinks available for paid plans only.",
+      "web_url":"https:\/\/www.vudu.com\/content\/movies\/details\/content\/531075",
+      "format":"4K",
+      "price":7.99,
+      "seasons":null,
+      "episodes":null
+    },
+    {
+      "source_id":24,
+    "name":"Amazon",
+    "type":"rent",
+    "region":"US",
+    "ios_url":"Deeplinks available for paid plans only.",
+    "android_url":"Deeplinks available for paid plans only.",
+    "web_url":"https:\/\/watch.amazon.com\/detail?gti=amzn1.dv.gti.aea9f774-1745-1035-3a21-2564dfd57ba3",
+    "format":"4K",
+    "price":3.99,
+    "seasons":null,
+    "episodes":null},
+    {
+      "source_id":398,
+      "name":"Microsoft Store",
+      "type":"rent",
+      "region":"US",
+      "ios_url":"Deeplinks available for paid plans only.",
+      "android_url":"Deeplinks available for paid plans only.",
+      "web_url":"https:\/\/www.microsoft.com\/en-us\/p\/the-amazing-spider-man-2\/8d6kgwzm0mrw",
+      "format":"4K",
+      "price":3.99,
+      "seasons":null,
+      "episodes":null},
+    {
+      "source_id":140,
+      "name":"Google Play",
+      "type":"buy",
+      "region":"US",
+      "ios_url":"Deeplinks available for paid plans only.",
+      "android_url":"Deeplinks available for paid plans only.",
+      "web_url":"https:\/\/play.google.com\/store\/movies\/details?id=rFN3fwhcdlE",
+      "format":"4K",
+      "price":9.99,
+      "seasons":null,
+      "episodes":null
+    }
+  ]
+}
+
 const getTitleInformation = async title => {
   if (!title) return
   // IMDb API 'SeachTitle' endpoint
   let response = await fetch(`https://imdb-api.com/en/API/SearchTitle/k_vd8o64bn/${title.trim()}`)
   let data = await response.json()
-  
   // No data is found matching query
   if (!data.results.length) {
     console.log('No matches found')
     return
     // TODO: Display message on page. 'No matches found with that title.'
   }
-
   displaySearchResults(data)
 }
 
@@ -37,9 +167,13 @@ function renderFavorites() {
     var li = document.createElement("li");
     li.setAttribute("data-index", i);
     li.textContent = title;
-    var button = document.createElement("button");
-    button.textContent = "Remove";
+    var button = document.createElement("button")
     button.classList = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded ml-50";
+  
+    button.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" style="fill: #FFF;" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg>
+    `
+
     li.appendChild(button);
     favoritesList.appendChild(li);
   }
@@ -49,7 +183,7 @@ function renderFavorites() {
 favoriteBtn.addEventListener("click", function(){
   var titleText = titleSpan.textContent.trim();
 
-  if(titleText === ""){
+  if (titleText === ""){
     return;
   }
   favorites.push(titleText);
@@ -58,7 +192,7 @@ favoriteBtn.addEventListener("click", function(){
 })
 
 
-favoritesList.addEventListener("click", function(event) {
+favoritesList.addEventListener("click", function (event) {
     var element = event.target;
     
     if (element.matches("button") === true) {
@@ -70,12 +204,6 @@ favoritesList.addEventListener("click", function(event) {
     }
 })
 
-  
-
-
-
-
-
 const reduceSources = () => {
   let sources = []
   let sourceNames = [] 
@@ -86,12 +214,29 @@ const reduceSources = () => {
       sources.push(source)
     }
   })
-
   titleInfo.sources = sources
 }
 
-const updateScreen = () => {
+const updateScreen = async () => {
   document.querySelector('#title').textContent = titleInfo.title
+  titleInfo.similar_titles.slice(0, 5).forEach(async (title, i) => {
+    console.log(title)
+    // let first = await fetch(`https://api.watchmode.com/v1/title/${title}/details/?apiKey=${watchModeKey}&append_to_response=sources`)
+    // let firstData = await first.json()
+    let firstData = testData
+    let response = await fetch(`https://imdb-api.com/en/API/SearchTitle/k_vd8o64bn/${firstData.imdb_id}`)
+
+    let data = await response.json()
+    const image = document.createElement('img')
+    image.setAttribute('src', data.results[0].image)
+    image.classList = 'cursor-pointer'
+    document.querySelector('.pic-ctn').append(image)
+    image.addEventListener('click', () => {
+      document.querySelector('.pic-ctn').innerHTML = ''
+      titleInfo = firstData
+      updateScreen()
+    })
+  })
 }
 
 const displaySearchResults = data => {
@@ -136,10 +281,7 @@ function init(){
   if (savedFavorites !== null){
     favorites = savedFavorites;
   }
-  
   renderFavorites();
 }
-
-
 
 init();

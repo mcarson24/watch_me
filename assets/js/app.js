4dda0390c016ba245de3c50d372304b59ba27633
Matthew Carson <matthew.carson@pm.me>
2022-04-01 05:46:34 -0400

    Lots more forgot to commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,9 +23,11 @@ const testData = {
   "tmdb_id":102382,
   "tmdb_type":"movie",
   "genres":[1,2,9],
-  "genre_names":["Action",
-  "Adventure",
-  "Fantasy"],
+  "genre_names":[
+    "Action",
+    "Adventure",
+    "Fantasy"
+  ],
   "user_rating":6.6,
   "critic_score":52,
   "us_rating":"PG-13",
@@ -160,15 +162,16 @@ function saveToFavorites(){
 }
 
 function renderFavorites() {
-
   favoritesList.innerHTML = "";
   for (var i = 0; i < favorites.length; i++) {
-    var title = favorites[i];
     var li = document.createElement("li");
     li.setAttribute("data-index", i);
-    li.textContent = title;
+    li.dataset.id = favorites[i].id
+    li.textContent = favorites[i].title;
+    li.classList = 'm-2'
+
     var button = document.createElement("button")
-    button.classList = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded ml-50";
+    button.classList = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-2 h-12 border border-blue-700 rounded ml-50";
   
     button.innerHTML = `
       <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" style="fill: #FFF;" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg>
@@ -181,12 +184,13 @@ function renderFavorites() {
 }
 
 favoriteBtn.addEventListener("click", function(){
-  var titleText = titleSpan.textContent.trim();
+  if (!titleInfo.title) return
 
-  if (titleText === ""){
-    return;
-  }
-  favorites.push(titleText);
+  favorites.push({
+    id: titleInfo.id,
+    title: titleInfo.title
+  });
+
   saveToFavorites();
   renderFavorites();
 })
@@ -194,14 +198,15 @@ favoriteBtn.addEventListener("click", function(){
 
 favoritesList.addEventListener("click", function (event) {
     var element = event.target;
+
+    if (element.matches('ul') || element.matches('li')) return
+
+    var index = element.parentElement.getAttribute("data-index");
+    console.log(index)
+    favorites.splice(index, 1);
     
-    if (element.matches("button") === true) {
-      var index = element.parentElement.getAttribute("data-index");
-      favorites.splice(index, 1);
-      
-      saveToFavorites();
-      renderFavorites();
-    }
+    saveToFavorites();
+    renderFavorites();
 })
 
 const reduceSources = () => {
@@ -219,24 +224,31 @@ const reduceSources = () => {
 
 const updateScreen = async () => {
   document.querySelector('#title').textContent = titleInfo.title
-  titleInfo.similar_titles.slice(0, 5).forEach(async (title, i) => {
-    console.log(title)
-    // let first = await fetch(`https://api.watchmode.com/v1/title/${title}/details/?apiKey=${watchModeKey}&append_to_response=sources`)
-    // let firstData = await first.json()
-    let firstData = testData
-    let response = await fetch(`https://imdb-api.com/en/API/SearchTitle/k_vd8o64bn/${firstData.imdb_id}`)
-
-    let data = await response.json()
-    const image = document.createElement('img')
-    image.setAttribute('src', data.results[0].image)
-    image.classList = 'cursor-pointer'
-    document.querySelector('.pic-ctn').append(image)
-    image.addEventListener('click', () => {
-      document.querySelector('.pic-ctn').innerHTML = ''
-      titleInfo = firstData
-      updateScreen()
-    })
+  document.querySelector('#favorite-btn').classList.remove('hidden')
+  const availableServices = ['google_play', 'amazon', 'disney+', 'hulu', 'netflix', 'hbo_max']
+  // Remove previous search's steaming services
+  const services = document.getElementsByClassName('service')
+  for (const service of services) {
+    service.classList.add('hidden')
+    service.setAttribute('href', '#')
+  }
+  let hasAtLeastOneStreamer = false
+  titleInfo.sources.forEach(source => {
+    const formatted_source_name = source.name.toLowerCase().replace(' ', '_')
+    if (availableServices.includes(formatted_source_name)) {
+      hasAtLeastOneStreamer = true
+      const image = document.querySelector(`#${formatted_source_name}`)
+      image.classList.remove('hidden')
+      image.setAttribute('href', source.web_url)
+    }
+    document.querySelector('#message').textContent = ''
   })
+  if(!hasAtLeastOneStreamer) {
+    const message = document.createElement('p')    
+    message.textContent = `${titleInfo.title} is not currently available on any of the services we support.`
+    message.setAttribute('id', 'message')
+    document.querySelector('#streaming_services').append(message)
+  }
 }
 
 const displaySearchResults = data => {
@@ -273,6 +285,18 @@ const resetSearchResults = () => {
 searchForm.addEventListener('submit', e => {
   e.preventDefault()
   getTitleInformation(e.target.children[0].value)
+})
+
+favoritesList.addEventListener('click', async e => {
+  if (!e.target.matches('li')) return 
+  // WatchMode Title Details Endpoint
+  response = await fetch(`https://api.watchmode.com/v1/title/${e.target.dataset.id}/details/?apiKey=${watchModeKey}&append_to_response=sources`)
+  titleInfo = await response.json()
+  reduceSources()
+  
+  // Remove search results
+  resetSearchResults()
+  updateScreen()
 })
 
 function init(){
